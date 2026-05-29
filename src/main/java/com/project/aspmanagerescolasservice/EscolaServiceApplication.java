fa38187cae97513bbe19bae5632c242027acca91
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2026-05-29 01:01:48 -0300

    init

---
+++ src/main/java/com/project/aspmanagerescolasservice/EscolaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.aspmanagerescolasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/aspmanagerescolasservice/EscolaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aspmanagerescolasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
