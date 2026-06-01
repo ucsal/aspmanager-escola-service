a34772fcbc396f4dae0cecb8307713f408f54a9d
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2026-06-01 02:11:09 -0300

    feat: integrate microservice with eureka server

--- src/main/java/com/project/aspmanagerescolasservice/EscolaServiceApplication.java
+++ src/main/java/com/project/aspmanagerescolasservice/EscolaServiceApplication.java
@@ -2,8 +2,10 @@ package com.project.aspmanagerescolasservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EscolaServiceApplication {
 
 	public static void main(String[] args) {
