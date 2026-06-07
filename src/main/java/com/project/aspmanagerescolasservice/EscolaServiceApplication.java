9ab3028a460f5ac1dd5fdf51813371d4b6f7c820
GabrielFraga <87135402+Gab1203@users.noreply.github.com>
2026-06-07 14:48:36 -0300

    fix: Update API Gateway URL for production environment

--- src/main/java/com/project/aspmanagerescolasservice/EscolaServiceApplication.java
+++ src/main/java/com/project/aspmanagerescolasservice/EscolaServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 		description = "Microserviço de Gestão de Escolas, Instituições e Disciplinas"),
  	servers = {
         @Server(url = "http://localhost:8083", description = "Ambiente Local (Desenvolvimento)"),
-        @Server(url = "http://localhost:8080/escola", description = "API Gateway (Produção)")
+        @Server(url = "http://localhost:8080/api/v1/orq/escola", description = "API Gateway (Produção)")
     })
 @EnableDiscoveryClient
 public class EscolaServiceApplication {
